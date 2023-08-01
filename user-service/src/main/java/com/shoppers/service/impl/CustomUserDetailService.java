de94865961d06a09ff2a254727ce8aa431841f39
devendrasanghavi404 <devendras95710@gmail.com>
2023-08-01 21:21:23 +0530

    class:CustomUser

---
+++ user-service/src/main/java/com/shoppers/service/impl/CustomUserDetailService.java
@@ -0,0 +1,23 @@
+package com.shoppers.service.impl;
+
+import com.shoppers.model.UserEntity;
+import com.shoppers.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(username).
+                orElseThrow(()->new UsernameNotFoundException("User with given email not found"));
+        return user;
+    }
+}
